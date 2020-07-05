f8fdb5156207e8a99c454bd7366a41cfcf3fe6a8
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-05 15:48:32 +0100

    Edit documentation

---
+++ R/SCRCdata-package.R
@@ -0,0 +1,18 @@
+#' SCRCdata
+#'
+#' Functions to interact with the SCRC data pipeline
+#'
+#' @name SCRCdata-package
+#' @aliases SCRCdata
+#' @docType package
+#'
+#' @importFrom assertthat assert_that
+#' @import dplyr
+#' @import readr
+#' @import readxl
+#' @import reshape2
+#' @import SCRCdataAPI
+#' @import sf
+#' @import tibble
+#'
+NULL
