b4e5ff9b34092cdb3baf16f789546e325a2427a0
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-08 20:27:03 +0100

    Fix inputs

--- inst/scripts/ons_demographics.R
+++ inst/scripts/ons_demographics.R
@@ -1,7 +1,4 @@
-#' Small Area Population Estimates 2018. Estimated population by sex,
-#' single year of age, 2011 Data Zone area, and council area: 30 June 2018.
-#' (From: https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/population/population-estimates/2011-based-special-area-population-estimates/small-area-population-estimates/time-series)
-#'
+
 
 library(SCRCdataAPI)
 
@@ -9,23 +6,27 @@ library(SCRCdataAPI)
 download_source_version(dataset = "ons_demographics")
 
 # Process data and generate hdf5 file
-sourcefile <- c("data-raw/sape-2018-persons.xlsx",
-                "data-raw/sape-2018-females.xlsx",
-                "data-raw/sape-2018-males.xlsx")
+sourcefile <- c("data-raw/england_Females.csv",
+                "data-raw/england_Males.csv",
+                "data-raw/england_Persons.csv")
 
 h5filename <- "england_population.h5"
 
 output_area_sf <- file.path("data-raw", "outputarea_shapefile",
-                         "Output_Areas__December_2011__Boundaries_EW_BFC.shp")
+                            "Output_Areas__December_2011__Boundaries_EW_BFC.shp")
 
 oa_conversion_table <- file.path("data-raw", "oa_conversion_table.csv")
-grp.names <- c("dz", "ur", "iz", "la", "hb", "mmw", "spc", "grid1km",
-               "grid10km")
-full.names <- c("data zone", "urban rural classification",
-                "intermediate zone", "local authority",
-                "health board", "multi member ward",
-                "scottish parliamentary constituency", "grid area",
-                "grid area")
+grp.names <- c("OA", "EW", "LA", "LSOA", "MSOA", "CCG", "STP", "UA","LHB",
+               "grid1km", "grid10km")
+
+full.names <- c("output area", "electoral ward",
+                "local authority", "lower super output area",
+                "mid-layer super output area",
+                "clinical commissioning group",
+                "sustainability and transformation partnership",
+                "unitary authority", "local health board",
+                "grid area", "grid area")
+
 subgrp.names <- c("total", "1year", "5year", "10year",
                   "sg_deaths_scheme")
 age.classes <- list("total", 0:90, seq(0, 90, 5), seq(0, 90, 10),
