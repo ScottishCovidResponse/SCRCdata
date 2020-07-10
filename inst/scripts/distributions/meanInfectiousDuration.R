0f5e4a33e94195fc1339cc73e19ce14284996e9c
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-10 21:04:11 +0100

    generate toml file and push metadata to database associated with meanInfectiousDuration data product

---
+++ inst/scripts/distributions/meanInfectiousDuration.R
@@ -0,0 +1,56 @@
+#' meanInfectiousDuration
+#'
+
+library(SCRCdataAPI)
+library(dplyr)
+library(devtools)
+
+
+
+# initialise --------------------------------------------------------------
+
+key <- read.table("token.txt")
+
+# Data product
+
+dataset <- "meanInfectiousDuration"
+productStorageRoot <- "boydorr"
+path <- file.path("parameters", "meanInfectiousDuration", "meanInfectiousDuration.toml")
+namespace <- "SCRC"
+productVersion <- "0.1.0"
+filename <- "meanInfectiousDuration.toml"
+
+
+# check -------------------------------------------------------------------
+
+# Check whether productStorageRoot exists in the registry
+if(!check_exists("storage_root", list(name = productStorageRoot))) {
+  storage_rootId <- new_storage_root(name = productStorageRoot,
+                                     root = "ftp://boydorr.gla.ac.uk/scrc/",
+                                     key = key)
+}
+
+# Check whether namespace exists in the registry
+if(!check_exists("namespace", list(name = namespace))) {
+  namespaceId <- new_namespace(name = namespace,
+                               key = key)
+}
+
+
+
+create_distribution(filename = "meanInfectiousDuration.toml",
+                    descriptor = "meanInfectiousDuration",
+                    distribution = "Lognormal",
+                    parameters = list(meanlog = 321.6))
+
+
+# data product ------------------------------------------------------------
+
+upload_data_product(storage_root = productStorageRoot,
+                    path = path,
+                    dataset = dataset,
+                    filename = filename,
+                    version = productVersion,
+                    key = key)
+
+
