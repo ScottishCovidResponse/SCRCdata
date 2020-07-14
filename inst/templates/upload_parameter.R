e0f1289a37f3872c46314cac2231a14b8fe785ac
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-14 18:06:16 +0100

    template for uploading metadata associated with made up parameters

---
+++ inst/templates/upload_parameter.R
@@ -0,0 +1,53 @@
+#' parameter-name
+#'
+
+library(SCRCdataAPI)
+
+# ** note that you have to upload the file to GitHub yourself! **
+
+
+# initialise --------------------------------------------------------------
+
+key <- read.table("token.txt")
+
+namespace <- "SCRC"
+name <- "human/infection/SARS-CoV-2/parameter-name"
+component_name <- gsub("^.*/([^/]*)$", "\\1", name)
+productVersion <- "0.1.0"
+component_value <- 999.99
+productStorageRoot <- "DataRepository"
+
+
+# default data that should be in database ---------------------------------
+
+storage_rootId <- new_storage_root(
+  name = productStorageRoot,
+  root = "https://raw.githubusercontent.com/ScottishCovidResponse/DataRepository/",
+  key = key)
+
+namespaceId <- new_namespace(name = namespace,
+                             key = key)
+
+
+# generate toml -----------------------------------------------------------
+
+path <- paste("master", namespace, name, sep = "/")
+filename <- paste0(productVersion, ".toml")
+
+create_estimate(filename = filename,
+                path = file.path("data-raw", path),
+                parameters = as.list(setNames(component_value, component_name)))
+
+
+# upload data product metadata to database --------------------------------
+
+upload_data_product(storage_root_id = storage_rootId,
+                    path = path,
+                    name = name,
+                    component_name = NA,
+                    filename = filename,
+                    version = productVersion,
+                    namespace_id = namespaceId,
+                    key = key)
+
+
