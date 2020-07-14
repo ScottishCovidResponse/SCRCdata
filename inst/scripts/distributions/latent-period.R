27b5d4059b9bc498f5627eac28279ffc97785fec
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-14 11:17:09 +0100

    update scripts to work with SCRCdataAPI and rename them

---
+++ inst/scripts/distributions/latent-period.R
@@ -0,0 +1,54 @@
+#' latent-period
+#'
+
+library(SCRCdataAPI)
+library(dplyr)
+library(devtools)
+
+
+# initialise --------------------------------------------------------------
+
+key <- read.table("token.txt")
+
+namespace <- "SCRC"
+name <- "human/infection/SARS-CoV-2/latent-period"
+component_name <- gsub("^.*/([^/]*)$", "\\1", name)
+productVersion <- "0.1.0"
+component_value <- 123.12
+productStorageRoot <- "DataRepository"
+
+
+# default data that should be in database ---------------------------------
+
+storage_rootId <- new_storage_root(
+  name = productStorageRoot,
+  root = "https://raw.githubusercontent.com/ScottishCovidResponse/DataRepository/",
+  key = key)
+
+namespaceId <- new_namespace(name = namespace,
+                             key = key)
+
+
+# generate toml -----------------------------------------------------------
+
+path <- paste("master", namespace, name, sep = "/")
+filename <- paste0(productVersion, ".toml")
+
+create_estimate(filename = filename,
+                path = file.path("data-raw", path),
+                value = component_value,
+                name = component_name)
+
+
+# data product ------------------------------------------------------------
+
+upload_data_product(storage_root_id = storage_rootId,
+                    path = path,
+                    component_name = component_name,
+                    filename = filename,
+                    version = productVersion,
+                    namespace_id = namespaceId,
+                    key = key)
+
+
+
---
+++ inst/scripts/distributions/symptom-delay.R
@@ -0,0 +1,57 @@
+#' symptom-delay
+#'
+
+library(SCRCdataAPI)
+library(dplyr)
+library(devtools)
+
+
+# initialise --------------------------------------------------------------
+
+key <- read.table("token.txt")
+
+namespace <- "SCRC"
+name <- "human/infection/SARS-CoV-2/symptom-delay"
+component_name <- gsub("^.*/([^/]*)$", "\\1", name)
+productVersion <- "0.1.0"
+disctribution <- "Gaussian"
+parameters <- list(mean = -16.08,
+                   SD = 30)
+productStorageRoot <- "DataRepository"
+
+
+# default data that should be in database ---------------------------------
+
+storage_rootId <- new_storage_root(
+  name = productStorageRoot,
+  root = "https://raw.githubusercontent.com/ScottishCovidResponse/DataRepository/",
+  key = key)
+
+namespaceId <- new_namespace(name = namespace,
+                             key = key)
+
+
+# generate toml -----------------------------------------------------------
+
+path <- paste("master", namespace, name, sep = "/")
+filename <- paste0(productVersion, ".toml")
+
+create_distribution(filename = filename,
+                    path = file.path("data-raw", path),
+                    descriptor = component_name,
+                    distribution = distribution,
+                    parameters = parameters)
+
+
+# data product ------------------------------------------------------------
+
+upload_data_product(storage_root_id = storage_rootId,
+                    path = path,
+                    component_name = component_name,
+                    filename = filename,
+                    version = productVersion,
+                    namespace_id = namespaceId,
+                    key = key)
+
+
+
