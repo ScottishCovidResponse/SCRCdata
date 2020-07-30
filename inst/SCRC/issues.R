41d5f8fa02c4212f6bc158ea0d2ee21d40b88a8c
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-30 14:48:34 +0100

    move all the scripts

---
+++ inst/SCRC/issues.R
@@ -0,0 +1,9 @@
+
+data_product <- "records/SARS-CoV-2/scotland/cases_and_management"
+object_components <- c("testing_location/date-cumulative")
+
+
+attach_issue(object_component = "testing_location/date-cumulative",
+             severity = 19,
+             description = "Data dump caused a spike on the 15th of June",
+             key)
