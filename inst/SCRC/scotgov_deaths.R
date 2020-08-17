b6b4c08ef20dbeb11cbb46c32ec8e9dad36e7b71
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-08-17 16:04:53 +0100

    change version number

--- inst/SCRC/scotgov_deaths.R
+++ inst/SCRC/scotgov_deaths.R
@@ -59,7 +59,7 @@ WHERE {
 # where is the processing script stored?
 repo_storageRoot <- "github"
 script_gitRepo <- "ScottishCovidResponse/SCRCdata"
-repo_version <- "0.1.1"
+repo_version <- "0.1.2"
 processing_script <- "scotgov_deaths.R"
 
 
