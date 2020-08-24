ea18150496191fcecf6e7f03eb78c69c9d0d1ca8
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-08-24 12:34:15 +0100

    use journal_names object from SCRCdata package

--- inst/SCRC/papers/litcovid.R
+++ inst/SCRC/papers/litcovid.R
@@ -2,11 +2,10 @@
 #'
 
 library(dplyr)
-
+data("journal_names", package = "SCRCdata")
 
 version <- "1.0.0"
 key <- readLines("token/token.txt")
-
 
 
 # Download *.ris file and convert to dataframe
@@ -16,42 +15,8 @@ download.file(path, "data-raw/litcovid.ris")
 dat <- revtools::read_bibliography("data-raw/litcovid.ris") %>%
   dplyr::rename(abbreviation = journal)
 
-
-# Download file to convert journal titles to abbreviations
-download.file(file.path("ftp://ftp.ncbi.nih.gov", "pubmed", "J_Medline.txt"),
-              "conversion_table.txt")
-pubmed_journals <- readLines("conversion_table.txt")
-
-
-# Generate conversion table
-conversion_table <- lapply(seq_len(length(pubmed_journals)/8),
-                           function(i) {
-                             tmp <- pubmed_journals[(1+((i-1)*8)):(i*8)]
-                             journal <- gsub("JournalTitle: ", "", tmp[3])
-                             abbreviation <- gsub("MedAbbr: ", "", tmp[4])
-                             data.frame(journal = journal,
-                                        abbreviation = abbreviation)
-                           }) %>% do.call(rbind.data.frame, .) %>%
-  dplyr::filter(abbreviation != "") %>%
-  unique()
-
-# Remove square brackets (the data registry doesn't like them)
-conversion_table <- conversion_table %>%
-  dplyr::mutate(journal = gsub("\\[", "", journal),
-                journal = gsub("\\]", "", journal))
-
-
-# Some abbreviations in conversion_table represent multiple journal titles
-# Remove these from the conversion_table
-ind <- which(duplicated(conversion_table$abbreviation) |
-               duplicated(conversion_table$abbreviation, fromLast = T))
-problem_abbreviations <- sort(unique(conversion_table$abbreviation[ind]))
-conversion_table <- conversion_table %>%
-  filter(!abbreviation %in% problem_abbreviations)
-
-
 # Merge ris file data with conversion table
-merge_dat <- merge(dat, conversion_table, all.x = TRUE)
+merge_dat <- merge(dat, journal_names, all.x = TRUE)
 assertthat::assert_that(nrow(merge_dat) == nrow(dat))
 
 
@@ -151,13 +116,3 @@ for(i in seq_len(nrow(upload_these))) {
                version = version,
                key = key)
 }
-
-
-
-
-
-
-
-
-
-
