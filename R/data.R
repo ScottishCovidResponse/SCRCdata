27ac514ed5ec922197c01efbeffa847b02b1ceb1
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-08-24 12:33:57 +0100

    install journal names abbreviation conversion table as data object

--- R/data.R
+++ R/data.R
@@ -1,4 +1,12 @@
-#' Conversion tables for journal names and abbreviations
+#' Conversion table for journal names and abbreviations
+#'
+#' A dataset containing a conversion table for journal names and abbreviations
+#'
+#' @format A data frame with 2 columns
+#' \describe{
+#'   \item{journal}{full journal name}
+#'   \item{abbreviation}{journal name abbreviation}
+#' }
 #'
 #' @docType data
 #'
