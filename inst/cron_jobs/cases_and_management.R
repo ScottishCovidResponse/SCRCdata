dec42b5a0cfd2fa8e7ee88dd29c3595432b56a5f
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-09-03 13:11:37 +0100

    add cron job scripts

---
+++ inst/cron_jobs/cases_and_management.R
@@ -0,0 +1,14 @@
+library(devtools)
+
+# Download and install the new versions of SCRCdataAPI and SCRCdata
+install_github("ScottishCovidResponse/SCRCdataAPI")
+install_github("ScottishCovidResponse/SCRCdata")
+library(SCRCdataAPI)
+library(SCRCdata)
+
+# Find submission script
+submission_script <- system.file("SCRC/scotgov_management.R",
+                                 package = "SCRCdata")
+
+# Run submission script
+source(submission_script)
