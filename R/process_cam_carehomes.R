dabefa0bf382a1a4b6adec87dc061c0b9a9a4af9
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-09-07 18:54:50 +0100

    try to fix error with dplyr running remotely

--- R/process_cam_carehomes.R
+++ R/process_cam_carehomes.R
@@ -12,7 +12,7 @@
 process_cam_carehomes <- function(sourcefile, filename) {
 
   # Read in data
-  scotMan <- read.csv(file = sourcefile) %>%
+  scotMan <- read.csv(file = sourcefile, stringsAsFactors = F) %>%
     dplyr::mutate(featurecode = gsub(
       "http://statistics.gov.scot/id/statistical-geography/",
       "", featurecode),
