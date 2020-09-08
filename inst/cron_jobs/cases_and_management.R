82d18a0821cbb5b7ceeb844b150b08ac0506abdd
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-09-08 12:07:06 +0100

    update cron script

--- inst/cron_jobs/cases_and_management.R
+++ inst/cron_jobs/cases_and_management.R
@@ -10,27 +10,11 @@ install_github("ScottishCovidResponse/SCRCdata")
 library(SCRCdataAPI)
 library(SCRCdata)
 
-# Make a separate h5 file for each of these data sets
-datasets <- c("carehomes", "hospital", "mortality",
-              "nhsworkforce", "schools", "testing")
 
-for(i in seq_along(datasets)) {
-  # Find submission script
-  filename <- paste0("cam_", datasets[i], ".R")
-  submission_script <- system.file(file.path("SCRC", filename),
-                                   package = "SCRCdata")
+# Find submission script
+submission_script <- system.file(file.path("SCRC", "cases_and_management.R"),
+                                 package = "SCRCdata")
 
-  # Run submission script
-  source(submission_script)
+# Run submission script
+source(submission_script)
 
-  # # Get file names
-  # path <- file.path("srv", "ftp", "scrc", "records", "SARS-CoV-2",
-  #                   "scotland", "cases-and-management", datasets[i])
-  # files <- dir(path, full.names = TRUE)
-  #
-  # # Move files
-  # for(j in seq_along(files)) {
-  #   file.copy(from = files[j], to = file.path("/", path))
-  #   file.remove(path[j])
-  # }
-}
