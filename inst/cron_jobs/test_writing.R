97ae7cd08da9b672cbd1c03081d37967061b5b51
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-09-10 10:41:10 +0100

    try downloading a file

--- inst/cron_jobs/test_writing.R
+++ inst/cron_jobs/test_writing.R
@@ -1,1 +1,4 @@
 cat("writing works", file = "/srv/ftp/scrc/records/SARS-CoV-2/scotland/cases-and-management/test.txt")
+
+
+download.file("https://www.opendata.nhs.scot/dataset/b318bddf-a4dc-4262-971f-0ba329e09b87/resource/7fad90e5-6f19-455b-bc07-694a22f8d5dc/download/total_cases_by_hb_20200909.csv", "/srv/ftp/scrc/records/SARS-CoV-2/scotland/cases-and-management/test.csv")
