fcf0323a16aea10225ca344e0ae98df757584bcd
ciaranmcmonagle <c.mcmonagle.1@research.gla.ac.uk>
2020-10-06 22:03:18 +0100

    Revert "Merge branch 'master' of https://github.com/ScottishCovidResponse/SCRCdata"
    This reverts commit db6e2acb7b182c3d75de2e4f59f2a9b541ef4e6e, reversing
    changes made to bacb4171ffa9ba79279886fca33698b6e4f6a6a0.

--- R/process_ukgov_eng_lookup.R
+++ R/process_ukgov_eng_lookup.R
@@ -68,62 +68,133 @@ process_ukgov_eng_lookup <- function(sourcefile,
   # Read in datazone shapefile and check for non-intersecting geometries
   output_areas <- sf::st_read(output_area_sf, quiet = TRUE) %>%
     sf::st_make_valid() %>% rename(AREAcode=OA11CD)
-  # Read in UK national grid shapefile
-  grid_shp = sf::st_read(sourcefile[["grid_shapefile"]], quiet = TRUE) %>%
-    sf::st_make_valid() %>% rename("grid_id" = "PLAN_NO")
   
-  sh_unit <- sf::st_crs(output_areas, parameters = TRUE)$units_gdal
-  assertthat::assert_that(sh_unit == "metre",
-                          msg = "Unexpected CRS: shapefile distances should be in metres")
+  # Create grid cell intersections
+  gridsizes <- grid_names[grepl("^grid", grid_names)] %>%
+    lapply(function(x) gsub("grid", "", x) %>% gsub("km", "", .)) %>%
+    as.numeric()
   
-  ## From inside grid_intersection()
-  subdivisions <- sf::st_intersection(grid_shp, output_areas)
-  
-  # Remove cells which intersect simply because they touch the shapefile
-  # - These are reduced to lines or points in the intersection
-  is_polygon <- sapply(subdivisions$grids, function(x)
-    any(grepl("LINE|POINT", class(x))))
-  if(length(is_polygon)>0){
-    subdivisions <- subdivisions[!is_polygon, , drop = FALSE]
+  oa_subdivisions <- list()
+  grid_matrix <- list()
+  for (g in seq_along(gridsizes)) {
+    tmp <- SCRCdataAPI::grid_intersection(output_areas, gridsizes[g])
+    tag <- paste0("grid", gridsizes[g], "km")
+    oa_subdivisions[[g]] <- tmp$subdivisions
+    names(oa_subdivisions)[g] <- tag
+    grid_matrix[[g]] <- tmp$grid_matrix
+    names(grid_matrix)[g] <- tag
+    oa_subdivisions[[g]]$Datazone_component_id<-
+      c(seq_along(oa_subdivisions[[g]]$grid_id))
   }
   
-
-  #Check that datazone components add up to same area as original datazone
-  subdivision_area = data.frame(AREAcode = subdivisions$AREAcode,
-                                subd_area = st_area(subdivisions))
-  subdivision_area = subdivision_area %>%
-    group_by(.data$AREAcode) %>%
-    summarise(subd_area=sum(.data$subd_area))
-  datazone_area = data.frame(AREAcode = output_areas$AREAcode,
-                             dz_area = st_area(output_areas))
-  subdivision_area = subdivision_area %>%
-    left_join(datazone_area, by = "AREAcode")
-  subdivision_area$difference = subdivision_area$subd_area - subdivision_area$dz_area
-  assertthat::assert_that(all(round(as.numeric(subdivision_area$difference)) == 0))
-  subdivisions$Datazone_component_id = c(1:nrow(subdivisions))
-  
-  
-  # Make dataframes of the area of each datazone component in each grid cell
+  # Make dataframes of the area of each datazone comoonent in each grid cell
   # at both 1km and 10km
-  intersection_area <- data.frame(
-    grid1km_id = subdivisions$grid_id,
-    areagrid1km = as.numeric(sf::st_area(subdivisions)),
-    Datazone_component_id = subdivisions$Datazone_component_id,
-    AREAcode = subdivisions$AREAcode)
+  intersection_area1km <- data.frame(
+    grid1km_id = oa_subdivisions$grid1km$grid_id,
+    areagrid1km = as.numeric(sf::st_area(oa_subdivisions$grid1km)),
+    Datazone_component_id = oa_subdivisions$grid1km$Datazone_component_id,
+    AREAcode = oa_subdivisions$grid1km$AREAcode)
+  intersection_area10km <- data.frame(
+    grid10km_id = oa_subdivisions$grid10km$grid_id,
+    areagrid10km = as.numeric(sf::st_area(oa_subdivisions$grid10km)),
+    Datazone_component_id = oa_subdivisions$grid10km$Datazone_component_id,
+    AREAcode = oa_subdivisions$grid10km$AREAcode)
   
-  datazone_area$dz_area <- as.numeric(datazone_area$dz_area)
-  intersection_area <- intersection_area %>% left_join(datazone_area, "AREAcode")
-  conversion.table <- intersection_area %>% 
-    mutate(grid1km_area_proportion=areagrid1km/"dz_area")%>%
-    select("Datazone_component_id", "grid1km_area_proportion", "grid1km_id", "AREAcode")%>%
-    left_join(conversion.table, "AREAcode")
-  conversion.table <- conversion.table %>%
-    tibble::column_to_rownames("Datazone_component_id")
-  conversion.table[is.na(conversion.table)] <- 0
+  
+  #Make dataframe of datazone area
+  oa_area <- data.frame(AREAcode = output_areas$AREAcode,
+                        full_zone_area = as.numeric(sf::st_area(output_areas)))
+  
+  #Find which 1km grid cells are in each 10km grid cell
+  
+  gridslist=list(grid1km=vector(),
+                 grid10km=vector())
+  
+  #From inside grid_intersection, get sf object of grids
+  for(gridsize in gridsizes){
+    # Generate grid over bounding box of datazone shapefile
+    n <- gridsize*1000  # Assume gridsize given in km
+    grids <- sf::st_make_grid(sf::st_as_sfc(sf::st_bbox(output_areas)),
+                              cellsize = c(n, n))
+    
+    width <- sf::st_bbox(output_areas)$xmax - sf::st_bbox(output_areas)$xmin
+    height <- sf::st_bbox(output_areas)$ymax - sf::st_bbox(output_areas)$ymin
+    num_columns <- ceiling(width / n)
+    num_rows <- ceiling(height / n)
+    grid_labels <- paste0(1:num_columns, "-", rep(1:num_rows, each = num_columns))
+    
+    grid_matrix <- strsplit(grid_labels, "-") %>% lapply(as.numeric) %>% do.call(rbind.data.frame, .)
+    colnames(grid_matrix) <- c("x", "y")
+    
+    assertthat::assert_that(max(grid_matrix$x) == num_columns)
+    assertthat::assert_that(max(grid_matrix$y) == num_rows)
+    
+    gridslist[[paste0("grid",gridsize,"km")]] <- sf::st_sf(grids, grid_id = grid_labels)
+  }
+  
+  #Use sf objects to map which 1km cells are inside each 10km cell
+  covered_grids=st_covered_by(gridslist$grid1km,gridslist$grid10km,
+                              sparse = FALSE)
+  
+  #Extract this into a more useable format
+  grids.array=array(0, dim = c(100,length(gridslist$grid10km$grid_id)))
+  
+  for(i in seq_len(ncol(covered_grids))){
+    if(length(gridslist$grid1km$grid_id[which(covered_grids[,i]==TRUE)])<100){
+      grids.array[,i]=c(gridslist$grid1km$grid_id[which(covered_grids[,i]==TRUE)], rep(0, 100-length(gridslist$grid1km$grid_id[which(covered_grids[,i]==TRUE)])))
+    }else{
+      grids.array[,i]=gridslist$grid1km$grid_id[which(covered_grids[,i]==TRUE)]
+      
+    }
+  }
+  
+  colnames(grids.array)=gridslist$grid10k$grid_id
+  
+  grids.array = as.data.frame(grids.array)
+  
+  grid_lookup=reshape2::melt(grids.array, id.vars = NULL, variable.name = "grid10km_id", 
+                             value.name = "grid1km_id")
+  grid_lookup=grid_lookup[-which(grid_lookup$grid1km_id==0),]
+  grid_lookup$grid10km_id=factor(grid_lookup$grid10km_id,
+                                 levels=gridslist$grid10k$grid_id)
+  grid_lookup=grid_lookup[order(grid_lookup$grid10km_id),]
+  grid_lookup$grid10km_id=as.character(grid_lookup$grid10km_id)
+  
+  #Which grid cells have output_areas in them?
+  present_grids_lookup=grid_lookup[match(intersection_area1km$grid1km_id,
+                                         grid_lookup$grid1km_id),]
+  
+  #Join grids to datazone component areas
+  grids_area=left_join(present_grids_lookup,intersection_area1km,
+                       by="grid1km_id")
+  grids_area = left_join(grids_area, oa_area, by="AREAcode")
+  grids_area$prop=grids_area$areagrid1km/grids_area$full_zone_area
+  grids_area=grids_area[!duplicated(grids_area$Datazone_component_id),]
+  check_proportions=grids_area %>% 
+    group_by(AREAcode) %>% 
+    summarise(total_prop=sum(prop))
+  
+  assertthat::assert_that(all((round(check_proportions$total_prop,
+                                     digits=2)!=1)==FALSE))
+  
+  grids_area=left_join(grids_area,intersection_area10km,
+                       by=c("grid10km_id","AREAcode"))
+  
+  #Find proportion of each datazone in each grid cell
+  grids_area = grids_area %>%
+    dplyr::mutate("grid1km_area_proportion" = areagrid1km / full_zone_area,
+                  "grid10km_area_proportion" = areagrid10km / full_zone_area)%>%
+    dplyr::select(grid1km_id, grid1km_area_proportion, 
+                  grid10km_id,grid10km_area_proportion,AREAcode)
+  
+  conversion.table = left_join(grids_area,conversion.table, by="AREAcode")
+  conversion.table$oa_component_id=c(seq_along(conversion.table$grid1km_id))
+  conversion.table = conversion.table %>% tibble::column_to_rownames("oa_component_id")
+  conversion.table[is.na(conversion.table)]=0
   
   SCRCdataAPI::create_table(filename = h5filename,
                             path = path,
-                            component = "conversiontable/scotland",
+                            component = "conversiontable/englandwales",
                             df = conversion.table,
                             row_names = rownames(conversion.table),
                             column_units = colnames(conversion.table))
--- README.md
+++ README.md
@@ -6,5 +6,19 @@
 
 This is a git repository to store data processing scripts for the SCRC data pipeline. Processing functions used to convert raw data (e.g. csv files) into data products (e.g. h5 files) are stored in the `R` directory. Submission scripts used upload metadata associated with each dataset (original source, raw data location, processing script location, and data product location) to the data registry are stored in `inst/scripts`. Templates that can used to generate more of these scripts are stored in `inst/templates`.
 
+**Please read the [Wiki pages](https://github.com/ScottishCovidResponse/SCRCdata/wiki)
+ (scroll back up, fifth tab from the left) for more information, including:**
 
-For more information, please go to https://scottishcovidresponse.github.io/
+* [Installation instructions](https://github.com/ScottishCovidResponse/SCRCdata/wiki)
+* [Datasets](https://github.com/ScottishCovidResponse/SCRCdata/wiki/Datasets)
+* [Submission scipt templates](https://github.com/ScottishCovidResponse/SCRCdata/wiki/Submission-script-templates)
+* [Example workflow (how to upload metadata to the data registry)](https://github.com/ScottishCovidResponse/SCRCdata/wiki/Workflow)
+* [Versioning and filenames](https://github.com/ScottishCovidResponse/SCRCdata/wiki/Versioning-and-filenames)
+* [SCRCdataAPI::download_from_database() example](https://github.com/ScottishCovidResponse/SCRCdata/wiki/download_from_database()-example)
+* [SCRCdataAPI::download_from_url() example](https://github.com/ScottishCovidResponse/SCRCdata/wiki/download_from_url()-example)
+* [Useful links](https://github.com/ScottishCovidResponse/SCRCdata/wiki/Useful-links)
+
+NEW!
+
+* [HDF5 components](https://github.com/ScottishCovidResponse/SCRCdata/wiki/HDF5-components)
+* [toml components](https://github.com/ScottishCovidResponse/SCRCdata/wiki/TOML-components)
